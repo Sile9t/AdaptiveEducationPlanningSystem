11d590f0ddeaa8ee1031efbe8692f0524a42349a
Sile9t <eyesless1jack@gmail.com>
2025-05-13 22:05:55 +0300

    UPDATE: added Branch moonshine resource and injected it's in dashboard menu

---
+++ app/MoonShine/Resources/BranchResource.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\MoonShine\Resources;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Branch;
+
+use MoonShine\Resources\ModelResource;
+use MoonShine\Decorations\Block;
+use MoonShine\Fields\ID;
+use MoonShine\Fields\Field;
+use MoonShine\Components\MoonShineComponent;
+use MoonShine\Fields\Text;
+
+/**
+ * @extends ModelResource<Branch>
+ */
+class BranchResource extends ModelResource
+{
+    protected string $model = Branch::class;
+
+    protected string $title = 'Branch';
+
+    /**
+     * @return list<MoonShineComponent|Field>
+     */
+    public function fields(): array
+    {
+        return [
+            Block::make([
+                ID::make()->sortable(),
+                Text::make('Name')->sortable(),
+                Text::make('Description'),
+            ]),
+        ];
+    }
+
+    /**
+     * @param Branch $item
+     *
+     * @return array<string, string[]|string>
+     * @see https://laravel.com/docs/validation#available-validation-rules
+     */
+    public function rules(Model $item): array
+    {
+        return [];
+    }
+}
--- app/Providers/MoonShineServiceProvider.php
+++ app/Providers/MoonShineServiceProvider.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use App\MoonShine\Resources\BranchResource;
 use MoonShine\Providers\MoonShineApplicationServiceProvider;
 use MoonShine\MoonShine;
 use MoonShine\Menu\MenuGroup;
@@ -50,9 +51,13 @@ class MoonShineServiceProvider extends MoonShineApplicationServiceProvider
                 ),
             ]),
 
-            MenuItem::make('Documentation', 'https://moonshine-laravel.com/docs')
-                ->badge(fn() => 'Check')
-                ->blank(),
+            MenuGroup::make("", [
+                MenuItem::make('Branch', new BranchResource())->icon('heroicons.outline.user-group')
+            ]),
+
+            // MenuItem::make('Documentation', 'https://moonshine-laravel.com/docs')
+            //     ->badge(fn() => 'Check')
+            //     ->blank(),
         ];
     }
 
